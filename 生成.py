8d355af88cb723e958da9c591b57d57d75bd3a9d
Songchen Tan <i@tansongchen.com>
2019-11-30 11:59:00 -0800

    添加练习资料

--- 生成.py
+++ 生成.py
@@ -39,33 +39,33 @@ columns:
   - code
 ...
 '''
-f = open('资料/拆分表.txt', encoding = 'utf-8', mode = 'r')
+f = open('数据/拆分表.txt', encoding = 'utf-8', mode = 'r')
 l = [x.strip('\r\n').split('\t') for x in f]
 f.close()
-f = open('资料/简码表.txt', encoding = 'utf-8', mode = 'r')
+f = open('数据/简码表.txt', encoding = 'utf-8', mode = 'r')
 b = [x.strip('\r\n').split('\t') for x in f]
 f.close()
-f = open('资料/映射表.txt', encoding = 'utf-8', mode = 'r')
+f = open('数据/映射表.txt', encoding = 'utf-8', mode = 'r')
 d = {x.strip('\r\n').split('\t')[0]:x.strip('\r\n').split('\t')[1] for x in f}
 f.close()
-f = open('资料/映射表.txt', encoding = 'utf-8', mode = 'r')
+f = open('数据/映射表.txt', encoding = 'utf-8', mode = 'r')
 s = {x.strip('\r\n').split('\t')[0]:x.strip('\r\n').split('\t')[2] for x in f}
 for i in s:
     if s[i] == '':
         s[i] = i
 f.close()
-f = open('资料/特码表.txt', encoding = 'utf-8', mode = 'r')
+f = open('数据/特码表.txt', encoding = 'utf-8', mode = 'r')
 t = [x.strip('\r\n').split('\t') for x in f]
 f.close()
 u = [(x[0], (s[x[1]]+s[x[2]]+s[x[3]]+strB2Q(x[4].upper()))[:3]) for x in l if x[5] == '0']
 q = [(x[0], (d[x[1]]+d[x[2]]+d[x[3]]+x[4])[:3]) for x in l if x[5] == '0']
 qq = [(x[0], (d[x[1]]+d[x[2]]+d[x[3]]+x[4])[:3]) for x in l]
-f = open('输出/其他/全码表.txt', encoding = 'utf-8', mode = 'w')
+f = open('练习资料/全码表.txt', encoding = 'utf-8', mode = 'w')
 for i in q:
     f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
 qd = {x[0]:x[1] for x in q}
-f = open('输出/c_42.dict.yaml', encoding = 'utf-8', mode = 'w')
+f = open('方案/c_42.dict.yaml', encoding = 'utf-8', mode = 'w')
 f.write(c_42_meta)
 for i in b:
     snippet[i[0]] = {'prefix': i[1], 'body': i[0], 'description': i[0]}
@@ -74,7 +74,7 @@ for i in b:
 f.close()
 key = 'abcdefghijklmnopqrstuvwxyz;'
 a = [x + y + z for x in key for y in key for z in key]
-f = open('输出/c_42a.dict.yaml', encoding = 'utf-8', mode = 'w')
+f = open('方案/c_42a.dict.yaml', encoding = 'utf-8', mode = 'w')
 f.write(c_42a_meta)
 c3 = []
 bc = [i[0] for i in b]
@@ -93,7 +93,7 @@ for i in t:
 f.close()
 c = [(x[0], x[1]) for x in b if len(x[0]) == 1 and x[0] != '　']
 a = [[],[],[]]
-f = open('输出/opencc/brevity.txt', encoding = 'utf-8', mode = 'w')
+f = open('方案/opencc/brevity.txt', encoding = 'utf-8', mode = 'w')
 for i in c:
     if i[1] != qd[i[0]][:2]:
         if i[1][-1] in ',./':
@@ -106,16 +106,16 @@ for i in c:
         f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
 w = [(x[0], x[1]) for x in b if len(x[0]) == 2]
-f = open('输出/opencc/brevity2.txt', encoding = 'utf-8', mode = 'w')
+f = open('方案/opencc/brevity2.txt', encoding = 'utf-8', mode = 'w')
 for i in w:
     f.write(i[0] + '\t' + i[1] + '\n')
     a[2].append(i)
 f.close()
-f = open('输出/opencc/division.txt', encoding = 'utf-8', mode = 'w')
+f = open('方案/opencc/division.txt', encoding = 'utf-8', mode = 'w')
 for i in u:
     f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
-f = open('输出/其他/选重、无理码和简词集锦.txt', encoding = 'utf-8', mode = 'w')
+f = open('练习资料/选重、无理码和简词集锦.txt', encoding = 'utf-8', mode = 'w')
 f.write('选重：\n\n')
 for i in a[0]:
     f.write(i[0] + '\t' + i[1] + '\n')
@@ -130,7 +130,7 @@ f.close()
 dst = os.path.expanduser('~/Library/Rime/')
 l = ['c_42.dict.yaml', 'c_42a.dict.yaml', 'opencc/brevity.txt', 'opencc/brevity2.txt', 'opencc/division.txt']
 for i in l:
-    shutil.copyfile('输出/' + i, dst + i)
+    shutil.copyfile('方案/' + i, dst + i)
 
-with open(os.path.expanduser('~/Documents/243 - Workflow/Chai/.vscode/c.code-snippets'), 'w') as f:
-    json.dump(snippet, f, ensure_ascii=False, indent=2)
+# with open(os.path.expanduser('~/Documents/243 - Workflow/Chai/.vscode/c.code-snippets'), 'w') as f:
+#     json.dump(snippet, f, ensure_ascii=False, indent=2)
