2bc1ae87b79e9cd10a038d3a37be5cd8ef93e21e
tansongchen <tansongchen@users.noreply.github.com>
2024-02-09 20:46:08 +0000

    deploy: 03f9b787db872d7b2084a9d4ffab3371bf1a14c7

--- lua/c42/predictor.lua
+++ lua/c42/predictor.lua
@@ -22,12 +22,22 @@ end
 function this.func(translation, env)
   for candidate in translation:iter() do
     lib.yield(candidate)
+    if not env.engine.context:get_option("prediction") then
+      goto continue
+    end
     this.memory:user_lookup(candidate.text, false)
     for entry in this.memory:iter_user() do
       local phrase = lib.Phrase(this.memory, "user_table", candidate._start, candidate._end, entry)
       lib.yield(phrase:toCandidate())
     end
+    ::continue::
   end
+end
+
+---@param segment Segment
+---@param env Env
+function this.tags_match(segment, env)
+  return segment:has_tag("abc")
 end
 
 return this
